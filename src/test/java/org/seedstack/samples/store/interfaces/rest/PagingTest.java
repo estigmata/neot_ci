f5f005f817450dae97be27841d0e204b53508f72
Nestor Otondo <nestor.otondo@fundacion-jala.org>
2019-03-06 09:47:19 -0400

    Add Testing

---
+++ src/test/java/org/seedstack/samples/store/interfaces/rest/PagingTest.java
@@ -0,0 +1,29 @@
+package org.seedstack.samples.store.interfaces.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PagingTest {
+
+    @Test
+    public void getPageIndex() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void setPageIndex() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getPageSize() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void setPageSize() {
+        assertTrue(true);
+    }
+}
